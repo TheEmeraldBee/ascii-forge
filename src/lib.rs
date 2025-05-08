7081cb96491e5d7566960da7ac4d400262858fde
Brighton Cox <brightonlcox@gmail.com>
2025-05-07 21:47:36 -0600

    Removed crossterm from being exported

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,3 @@ pub mod math;
 pub mod widgets;
 
 pub mod prelude;
-
-// Export required crates
-pub use crossterm;
--- src/prelude.rs
+++ src/prelude.rs
@@ -4,9 +4,3 @@ pub use crate::math::*;
 pub use crate::render;
 pub use crate::renderer::{buffer::*, cell::*, render::*};
 pub use crate::window::*;
-
-pub use crossterm;
-
-pub use crossterm::cursor::*;
-pub use crossterm::event::*;
-pub use crossterm::style::*;
