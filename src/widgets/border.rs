3216049cb89c748b893b24b0be6b3e2b75c0c026
westbot <82739021+westbot657@users.noreply.github.com>
2025-09-29 02:11:41 -0600

    forgot to update uses of width/height

--- src/widgets/border.rs
+++ src/widgets/border.rs
@@ -25,6 +25,7 @@ impl Border {
         }
     }
 
+    // this is slightly faster than re-allocating the Border struct
     pub fn set_size(&mut self, size: impl Into<Vec2>) {
         self.size = size.into();
     }
@@ -32,20 +33,20 @@ impl Border {
 }
 impl Render for Border {
     fn render(&self, loc: Vec2, buffer: &mut Buffer) -> Vec2 {
-        for y in (loc.y + 1)..(loc.y + self.height - 1) {
+        for y in (loc.y + 1)..(loc.y + self.size.y - 1) {
             buffer.set(vec2(loc.x, y), self.vertical);
-            buffer.set(vec2(loc.x + self.width - 1, y), self.vertical);
+            buffer.set(vec2(loc.x + self.size.x - 1, y), self.vertical);
         }
 
         let _ = render!(buffer,
-            loc => [self.top_left, self.horizontal.repeat(self.width as usize - 2), self.top_right],
-            vec2(loc.x, loc.y + self.height - 1) => [self.bottom_left, self.horizontal.repeat(self.width as usize - 2), self.bottom_right]
+            loc => [self.top_left, self.horizontal.repeat(self.size.x as usize - 2), self.top_right],
+            vec2(loc.x, loc.y + self.size.y - 1) => [self.bottom_left, self.horizontal.repeat(self.size.x as usize - 2), self.bottom_right]
         );
 
         vec2(loc.x + 1, loc.y + 1)
     }
     fn size(&self) -> Vec2 {
-        vec2(self.width, self.height)
+        self.size
     }
 }
 
